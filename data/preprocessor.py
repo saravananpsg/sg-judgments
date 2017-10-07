9a8507652b53f64faec8d5e07ade1d6cc91881af
hetchkay <howkhang.lim@gmail.com>
2017-10-08 07:10:21 +0800

    1. Added code in data_wrangling notebook to generate cats.txt with catchword labels. 2. Added get_paras function in preprocessor.py

--- data/preprocessor.py
+++ data/preprocessor.py
@@ -1,11 +1,7 @@
-# -*- coding: utf-8 -*-
-
 import os
 import sys
 import bs4
 import re
-
-# from readability.readability import Document
 
 
 def get_title(soup):
@@ -16,48 +12,70 @@ def get_title(soup):
 
 def get_authors(soup):
     return ';'.join(
-        [" ".join(t.get_text().split())
-         for t in soup('p', class_=re.compile(
+        [" ".join(p.get_text().split())
+         for p in soup('p', class_=re.compile(
              r'[Jj]udg-[Aa]uthor'))
-         if t.get_text(strip=True) and
-         not any(s in t.get_text().lower()
+         if p.get_text(strip=True) and
+         not any(s in p.get_text().lower()
                  for s in ['introduction', 'background'])
          ])
 
 
 def get_case_details(soup):
     return '\t'.join(
-        [" ".join(t.get_text().replace('\n', ';').split())
-            for t in soup('td', class_='txt-body')
-            if t.get_text(strip=True) and
+        [" ".join(td.get_text().replace('\n', ';').split())
+            for td in soup('td', class_='txt-body')
+            if td.get_text(strip=True) and
             not any('Parties' in sibling.get_text() for sibling in
-                    t.find_previous_siblings()) and
-            'Editorial note' not in t.get_text()
+                    td.find_previous_siblings()) and
+            'Editorial note' not in td.get_text()
          ])
 
 
 def get_catchwords(soup):
     return ';'.join(
-        [" ".join(t.get_text().split())
-            for t in soup('p', class_='txt-body')
-            if t.get_text(strip=True)]).strip()
+        [" ".join(p.get_text().split())
+            for p in soup('p', class_='txt-body')
+            if p.get_text(strip=True)]).strip()
+
+
+def get_paras(html_code):
+    soup = bs4.BeautifulSoup(html_code, 'lxml')
+    paras = '\n\n'.join(' '.join(p.get_text().split())
+        for p in soup('p', class_=re.compile(r'[Jj]udg-([Qq]uote-)?[\d]+'))
+        if p.get_text(strip=True)
+        )
+    soup.decompose()
+    if not paras:
+        malformed_re = r'(<i>\s*<b>\s*<font.*?>)\s*(<p.*?>)\s*(</font>)(.+?)(</b>\s*</i>\s*</p>)'
+        html_code = re.sub(
+            malformed_re, r'\g<2>\g<1>\g<2>\g<4>\g<3>\g<5>', html_code)
+        soup = bs4.BeautifulSoup(html_code, 'lxml')
+        paras = soup.find('span', class_='txt-body').get_text()
+    return paras
 
 
 if __name__ == '__main__':
     try:
         for filename in os.listdir(sys.argv[1]):
-            file = os.path.join(sys.argv[1], filename)
+            html_file = os.path.join(sys.argv[1], filename)
+            text_file = os.path.join(sys.argv[3], filename.split('.')[0])
 
-            with open(file, 'r') as f_read:
-                soup = bs4.BeautifulSoup(f_read.read(), 'lxml')
+            with open(html_file, 'r', encoding='utf-8', errors='replace') as f:
+                html_code = f.read()
+                soup = bs4.BeautifulSoup(html_code, 'lxml')
 
-            with open(sys.argv[2], 'a') as f_write:
-                f_write.write(
+            with open(
+                sys.argv[2], 'a', encoding='utf-8', errors='replace') as f:
+                f.write(
                     "\t".join([filename.split('.')[0],
                                get_title(soup),
                                get_authors(soup),
                                get_case_details(soup),
-                               get_catchwords(soup)]).strip() +
-                    '\n')
+                               get_catchwords(soup)]).strip() + '\n')
+
+            with open(text_file, 'w', encoding='utf-8', errors='replace') as f:
+                f.write(get_paras(html_code))
+
     except Exception as e:
         raise e
